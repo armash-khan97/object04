c1824734b9feb224ae8395f25cb0af6bcde3be5e
Armash khan <79808235+armash-khan97@users.noreply.github.com>
2021-07-25 16:21:25 +0500

    ok

---
+++ app.js
@@ -0,0 +1,15 @@
+// function Professions(name, gender, address, education, profession){
+//     this.name = name,
+//     this.gender = gender,
+//     this.address = address,
+//     this.education = education,
+//     this.profession = profession
+// }
+// function getSubmit(){
+// var userName = document.getElementById("nameOne").value;
+// var userGender = document.getElementById("gender").value;
+// var userAddress = document.getElementById("address").value;
+// var userEducation = document.getElementById("education").value;
+// var userInput = new Professions(userName , userGender, userAddress, userEducation);
+// console.log(userInput);
+// }
